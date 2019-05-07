1635e80692965433a5e12788b0c5c22c150d977a
Jacob Garcia <janikgar@gmail.com>
2019-05-06 21:30:40 -0500

    working BE authorization

--- backend/app.ts
+++ backend/app.ts
@@ -1,38 +1,61 @@
 import * as express from "express";
 import * as OAuth2Strategy from "passport-oauth2";
-import * as session from "express-session";
 import * as axios from "axios";
-import { Moment } from "moment";
+import cookie_session = require('cookie-session');
 import { nest } from "../client-config.json";
 import passport = require('passport');
-import moment = require('moment');
 
 const app = express();
 const port = 3000;
 
-app.use(session({
-  secret: nest.client_secret,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: true }
+app.use(cookie_session({
+  maxAge: 1000 * 60 * 60 * 24 * 200,
+  secret: 'nest test'
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.use(new OAuth2Strategy({
-  authorizationURL: nest.auth_url,
-  clientID: nest.client_id,
-  clientSecret: nest.client_secret,
-  state: true,
-  tokenURL: nest.token_url,
-},
-(accessToken, refreshToken, profile, cb) => {
-  console.log(profile);
-  cb(null, profile);
-})
+    authorizationURL: nest.auth_url,
+    clientID: nest.client_id,
+    clientSecret: nest.client_secret,
+    state: true,
+    tokenURL: nest.token_url,
+  },
+  (accessToken, refreshToken, profile, done) => {
+    axios.default.get("https://developer-api.nest.com", {
+      headers: {
+        "Authorization" : `Bearer ${accessToken}`
+      }
+    }).then((res) => {
+      done(null, {
+        id: res.data.metadata.user_id,
+        token: accessToken,
+      });
+    });
+  })
 );
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+function isAuthenticated(req: express.Request, res: express.Response, next: express.NextFunction) {
+  console.log
+  if (req.user) {
+    next()
+  } else {
+    res.json({'error': 'user not authenticated'});
+  }
+}
 
 app.get("/", (_, res) => {
   res.json({"hello": "world"})
-  // res.send("Hello world");
 });
 
 app.get("/err/:error", (req, res) => {
@@ -44,26 +67,9 @@ app.get("/auth",
 );
 
 app.get("/auth/callback",
-  // passport.authenticate("oauth2"),
+  passport.authenticate("oauth2"),
   (req, res) => {
-    axios.default.post(
-      nest.token_url,
-      `code=${req.query.code}&client_id=${nest.client_id}&client_secret=${nest.client_secret}&grant_type=authorization_code`,
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      }
-    ).then(tokenRes => {
-      req.session.token = tokenRes.data.access_token;
-      req.session.expiration = parseInt(tokenRes.data.expires_in);
-      req.session.save(err => {
-        console.log(err);
-      })
-      res.json(req.session);
-    }).catch(tokenRes => {
-      res.status(400).json(tokenRes.response.data);
-    });
+    res.redirect('/getBasic')
   },
   (req, res) => {
     res.redirect(`/err/${req.body}`)
@@ -71,18 +77,19 @@ app.get("/auth/callback",
 )
 
 app.get("/getBasic",
+  isAuthenticated,
   (req, res) => {
     axios.default.get(
       "https://developer-api.nest.com/",
       { headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${req.session.token}`
+          "Authorization": `Bearer ${req.user.token}`
       }}
     ).then(response => {
-      res.send(response.data);
+      res.json(response.data);
     }).catch(error => {
-      // console.log(error);
-      // res.send(error);
+      console.log(error);
+      res.send(error);
     })
   }
 )
